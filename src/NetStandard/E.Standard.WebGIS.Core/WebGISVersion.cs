e87dae87dad2a33684c4093986e9f140ed16f396
jugstalt <gstalt@gmx.at>
2025-03-28 07:21:12 +0100

    version update

--- src/NetStandard/E.Standard.WebGIS.Core/WebGISVersion.cs
+++ src/NetStandard/E.Standard.WebGIS.Core/WebGISVersion.cs
@@ -6,7 +6,7 @@ public class WebGISVersion
 {
     public static Version Version
     {
-        get { return new Version(7, 25, 1204); }
+        get { return new Version(7, 25, 1209); }
     }
 
     public static string JsVersion
