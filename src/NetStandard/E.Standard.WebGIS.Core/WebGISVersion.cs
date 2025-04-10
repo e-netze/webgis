a1156eaf0fe4139c7443ca8eada1d5371e2624af
jugstalt <gstalt@gmx.at>
2025-04-10 08:40:34 +0200

    Release 7.25.1502

--- src/NetStandard/E.Standard.WebGIS.Core/WebGISVersion.cs
+++ src/NetStandard/E.Standard.WebGIS.Core/WebGISVersion.cs
@@ -6,7 +6,7 @@ public class WebGISVersion
 {
     public static Version Version
     {
-        get { return new Version(7, 25, 1401); }
+        get { return new Version(7, 25, 1502); }
     }
 
     public static string JsVersion
