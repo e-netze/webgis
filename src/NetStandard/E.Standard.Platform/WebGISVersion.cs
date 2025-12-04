08df9c5342f0b9ac4459a9d66b982483efc174ae
gis-eni <juergen.gstaltmaier@e-netze.at>
2025-12-04 11:12:47 +0100

    version update

--- src/NetStandard/E.Standard.Platform/WebGISVersion.cs
+++ src/NetStandard/E.Standard.Platform/WebGISVersion.cs
@@ -6,7 +6,7 @@ public class WebGISVersion
 {
     public static Version Version
     {
-        get { return new Version(7, 25, 4805); }
+        get { return new Version(7, 25, 4806); }
     }
 
     public static string JsVersion
