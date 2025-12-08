72c1f2bffefe9e6853e53f1b3d07d3915c296480
jugstalt <juergen.gstaltmaier@e-netze.at>
2025-12-08 15:41:01 +0100

    nuget updates: datlinq

--- src/NetStandard/E.Standard.Platform/WebGISVersion.cs
+++ src/NetStandard/E.Standard.Platform/WebGISVersion.cs
@@ -6,7 +6,7 @@ public class WebGISVersion
 {
     public static Version Version
     {
-        get { return new Version(7, 25, 4807); }
+        get { return new Version(7, 25, 4812); }
     }
 
     public static string JsVersion
